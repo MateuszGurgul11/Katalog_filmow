9f243eb467c143160b6fe2fb29819ef3441a8ba9
Mateusz <mateusz.gurgul.2003@gmail.com>
2024-03-08 14:07:06 +0100

    Dodanie aktorowww

--- movies-project/movies_catalogue/main.py
+++ movies-project/movies_catalogue/main.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 
 @app.context_processor
 def utility_processor():
-    def tmdb_image_url(poster_path, size="w342"):
-        return tmdb_client.get_poster_url(poster_path, size)
+    def tmdb_image_url(poster_path=None, backdrop_path=None, profile_path=None, size_p="w342", size_b="w780"):
+        if backdrop_path:
+            return tmdb_client.get_backdrop_url(backdrop_path, size_p)
+        elif poster_path:
+            return tmdb_client.get_poster_url(poster_path, size_b)
+        elif profile_path:
+            return tmdb_client.get_profile_url(profile_path)
+        else:
+            return None
     return {"tmdb_image_url": tmdb_image_url}
 
 @app.route("/")
@@ -14,9 +21,11 @@ def homepage():
     movies = tmdb_client.get_random_movies(how_many=8)
     return render_template("index.html", movies=movies)
 
-@app.route("/movie/<movie_id>")
+@app.route('/movie/<movie_id>')
 def movie_details(movie_id):
-    return render_template("movie_details.html")
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    return render_template("movie_details.html", movie=details, cast=cast)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- movies-project/movies_catalogue/tmdb_client.py
+++ movies-project/movies_catalogue/tmdb_client.py
@@ -3,6 +3,7 @@ from flask import Flask
 import tmdb_client
 import random
 
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTMyMWNiZmFmM2JkOTUzNmFhNjk2OTJjNjU2NGY3ZSIsInN1YiI6IjY1ZThiOTVmMzQ0YThlMDE0YTNlMjkwYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZURzHk4tXXmZmjHu67aZPFI5lMlx_PdANF_EbmKbjgY"
 app = Flask(__name__)
 
 @app.context_processor
@@ -13,16 +14,39 @@ def utility_processor():
 
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OTMyMWNiZmFmM2JkOTUzNmFhNjk2OTJjNjU2NGY3ZSIsInN1YiI6IjY1ZThiOTVmMzQ0YThlMDE0YTNlMjkwYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZURzHk4tXXmZmjHu67aZPFI5lMlx_PdANF_EbmKbjgY"
     headers = {
-        "Authorization": f"Bearer {api_token}"
+        "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
 def get_poster_url(poster_api_path, size = "w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
+
+def get_backdrop_url(backdrop_api_path, size="w780"):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{backdrop_api_path}"
+
+def get_profile_url(profile_api_path, size="w180"):
+    base_url ="https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{profile_api_path}"
 
 def get_movies(how_many):
     data = get_popular_movies()
@@ -32,5 +56,3 @@ def get_random_movies(how_many):
     data = get_popular_movies()
     movies = data["results"]
     return random.sample(movies, how_many)
-
-print(get_random_movies(5))
